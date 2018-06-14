93401be427b939c0ae03ef65de1b28abd983e3b0
Chandran <Preveen_Chandran@Dell.com>
2018-06-14 22:22:22 +0530

    Quick Changes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -4,16 +4,16 @@ import { SignInComponent } from './account/sign-in/sign-in.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { AddCategoryComponent } from './category/add-category/add-category.component';
 import {PwdChangeRequestComponent} from './account/pwd-change-request/pwd-change-request.component';
-import{PwdChangeResetComponent} from './account/pwd-change-reset/pwd-change-reset.component';
-import{ SuperCategoryComponent} from './category/super-category/super-category.component'
+import {PwdChangeResetComponent} from './account/pwd-change-reset/pwd-change-reset.component';
+import { SuperCategoryComponent} from './category/super-category/super-category.component'
 const routes: Routes = [
     { path: 'AddCategory', component: AddCategoryComponent },
     { path: 'Welcome', component: WelcomeComponent },
     { path: 'SignIn', component: SignInComponent },
     { path: 'Home', component: HomePageComponent },
     { path: 'PwdChangeReq', component: PwdChangeRequestComponent },
-    {path:'PwdChangeReset',component:PwdChangeResetComponent},
-    {path:'SuperCategory',component:SuperCategoryComponent},
+    {path: 'PwdChangeReset', component: PwdChangeResetComponent},
+    {path: 'SuperCategory', component: SuperCategoryComponent},
     { path: '', redirectTo: 'Home', pathMatch: 'full' },
     { path: '**', redirectTo: 'Home', pathMatch: 'full' }
 ];
