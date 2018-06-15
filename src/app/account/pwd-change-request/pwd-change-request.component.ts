8db35aaac03d37398fbdfb6bbc30fc670d3c794b
Chandran <Preveen_Chandran@Dell.com>
2018-06-15 15:50:17 +0530

    Quick Changes

--- src/app/account/pwd-change-request/pwd-change-request.component.ts
+++ src/app/account/pwd-change-request/pwd-change-request.component.ts
@@ -12,13 +12,24 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   styleUrls: ['./pwd-change-request.component.css'],
   animations: [
     trigger('heroState', [
-      state('inactive', style({
-      })),
-      state('active',   style({
-        transform: 'scale(1.05, 1.05)'
-      })),
+      state('inactive', style({transform: 'translateX(0) scale(1)'})),
+      state('active',   style({transform: 'translateX(0) scale(1.1)'})),
       transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
+      transition('active => inactive', animate('100ms ease-out')),
+      transition('void => inactive', [
+        style({transform: 'translateX(-100%) scale(1)'}),
+        animate(100)
+      ]),
+      transition('inactive => void', [
+        animate(100, style({transform: 'translateX(100%) scale(1)'}))
+      ]),
+      transition('void => active', [
+        style({transform: 'translateX(0) scale(0)'}),
+        animate(200)
+      ]),
+      transition('active => void', [
+        animate(200, style({transform: 'translateX(0) scale(0)'}))
+      ])
     ])
   ]
 })
