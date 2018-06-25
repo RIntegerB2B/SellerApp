cd351d378b49308bd82203d82c2fc7e2e86f08c5
Chandran <Preveen_Chandran@Dell.com>
2018-06-25 16:20:46 +0530

    Required Parameter

--- src/app/account/pwd-change-reset/pwd-change-reset.component.ts
+++ src/app/account/pwd-change-reset/pwd-change-reset.component.ts
@@ -4,6 +4,8 @@ import { PwdChangeReset } from './pwd-change-reset.model';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
 import { NavHeaderService } from '../../shared/nav-header/nav-header.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-pwd-change-reset',
@@ -14,14 +16,13 @@ export class PwdChangeResetComponent implements OnInit {
   resetPasswordForm: FormGroup;
   userModel: PwdChangeReset;
   emailKey: string;
-  
-  urlnomorevalid :boolean=false;
-  error :boolean=false;
+
+  urlnomorevalid: boolean;
+  error: boolean;
 
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router,
-    private navHeaderService: NavHeaderService) {
-    this.emailKey = "xyz"; // TODO: This value has to be pulled from Query String
-
+    private navHeaderService: NavHeaderService, private activatedRoute: ActivatedRoute) {
+    this.emailKey = this.activatedRoute.snapshot.paramMap.get('key');
   }
 
   ngOnInit() {
@@ -51,12 +52,12 @@ export class PwdChangeResetComponent implements OnInit {
       }
       if (value.indexOf("2") > -1) {
         // The Key didnt match , so show a message saying the URL is no more valid
-        this.urlnomorevalid=true;
-        
+        this.urlnomorevalid = true;
+
       }
       if (value.indexOf("0") > -1) {
         // Some exception happened. So show a message saying. Something went wrong pls try after sometime
-        this.error=true;
+        this.error = true;
       }
     }, error => {
       console.log(error);
