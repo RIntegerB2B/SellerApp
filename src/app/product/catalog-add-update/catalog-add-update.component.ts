bd9fcede50abd711155917aa84558405f1d99151
Chandran <Preveen_Chandran@Dell.com>
2018-07-12 18:18:16 +0530

    Catalog UI Change

--- src/app/product/catalog-add-update/catalog-add-update.component.ts
+++ src/app/product/catalog-add-update/catalog-add-update.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { NavHeaderService } from '../../shared/nav-header/nav-header.service';
 import { CatalogModel } from './catalog.model';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-catalog-add-update',
@@ -9,15 +11,40 @@ import { CatalogModel } from './catalog.model';
   styleUrls: ['./catalog-add-update.component.css']
 })
 export class CatalogAddUpdateComponent implements OnInit {
-
+  catalogForm: FormGroup;
+  catalogId: string;
   fileToUpload: File = null;
   reader: FileReader = new FileReader();
   catalogModel: CatalogModel = new CatalogModel();
   catalogImageBlob: Blob;
-  constructor(private productService: ProductService, private navHeaderService: NavHeaderService) { }
+  loadedCatalogModel: CatalogModel = new CatalogModel();
+  constructor(private fb: FormBuilder,
+    private productService: ProductService, private navHeaderService: NavHeaderService,
+    private activatedRoute: ActivatedRoute) {
+      this.catalogId = this.activatedRoute.snapshot.paramMap.get('key');
+    }
 
   ngOnInit() {
+    this.createForm();
     this.navHeaderService.hideMenuTransparent();
+    if (this.catalogId) {
+      this.loadCatalog(this.catalogId);
+    }
+  }
+
+  loadCatalog(catId: string) {
+    // loadedCatalogModel = getCatalog(catId);
+    this.catalogForm.setValue({
+      catalogName:    this.loadedCatalogModel.catalogName,
+      catalogImage: this.loadedCatalogModel.catalogImage
+   });
+  }
+
+  createForm() {
+    this.catalogForm = this.fb.group({
+      catalogName: ['', Validators.required],
+      catalogImage: ['']
+    });
   }
 
   handleFileInput(files: FileList, loadedImage) {
@@ -30,7 +57,6 @@ export class CatalogAddUpdateComponent implements OnInit {
       this.catalogImageBlob = new Blob([bytes.buffer]);
       const reader1 = new FileReader();
       reader1.readAsDataURL(this.catalogImageBlob);
-      console.log(this.catalogImageBlob);
       reader1.onload = function () {
         loadedImage.src = reader1.result;
       };
@@ -38,9 +64,12 @@ export class CatalogAddUpdateComponent implements OnInit {
     };
   }
 
-  save() {
-    this.catalogModel.catalogName = ''; // Get the data from the Template
+  catalogSave(catForm: FormGroup) {
+    this.catalogModel.catalogName =  catForm.controls.catalogName.value;
     this.catalogModel.catalogImage = this.catalogImageBlob;
+    this.productService.catalogCreate(this.catalogModel).subscribe(createdCatalog => {
+      console.log(createdCatalog);
+    });
   }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -3,12 +3,37 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { AppSetting } from '../config/appSetting';
+import { CatalogModel } from './catalog-add-update/catalog.model';
 
 @Injectable()
 export class ProductService {
-  
+  serviceUrl: string = AppSetting.serviceUrl;
+  headers: Headers = new Headers({
+    'Content-Type': 'application/json; charset=utf-8'
+  });
+  requestOptions: RequestOptions = new RequestOptions({ headers: this.headers });
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.log(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  constructor(private http: Http, private httpClient: HttpClient) { }
   superCategoryAdd(arg0: any): any {
     throw new Error('Method not implemented.');
+  }
+
+  catalogCreate(catalogModel: CatalogModel): Observable<CatalogModel> {
+    const calatalogUrl = 'catalog';
+    const url: string = this.serviceUrl + calatalogUrl;
+    return this.httpClient.post<CatalogModel>(url, catalogModel);
   }
 
 }
