110d4b479c9739ec51e9d3b3b21c8a6929da56b7
Chandran <Preveen_Chandran@Dell.com>
2018-07-17 18:39:41 +0530

    URL Issues

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,5 +1,5 @@
 export const AppSetting: AppSettingType = {
     awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/',
     local3010BuyerServiceUrl: 'http://localhost:3001/',
-    serviceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/ '
+    serviceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/'
 };
