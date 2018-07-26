279b4af23403a862d3958709ff64c158ffc7e2eb
Chandran <Preveen_Chandran@Dell.com>
2018-07-26 12:11:08 +0530

    Catalog Image Changes

--- src/app/shared/nav-header/nav-header.component.ts
+++ src/app/shared/nav-header/nav-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NavHeaderService} from '../../shared/nav-header/nav-header.service';
+import { NavHeaderService } from '../../shared/nav-header/nav-header.service';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-nav-header',
@@ -10,10 +11,13 @@ export class NavHeaderComponent implements OnInit {
   isMainCollapsed: Boolean = true;
   isMenMenuCollapsed: Boolean = true;
   menuVisible: boolean;
-  toggleMenuBar: string = 'colapseMenuBar';
-  constructor(public navHeaderService: NavHeaderService) { }
+  toggleMenuBar = 'colapseMenuBar';
+  readonly VAPID_PUBLIC_KEY = 'BIvwBoUek8ZLiE2HRr_srixb0Qi-Ql6CVBhhhvIuuZ5PMFYrfP0zSkNRrHD-uvIBhJ3_BDmzSFedMzu5ZuaVVRM';
+
+  constructor(public navHeaderService: NavHeaderService, private swUpdate: SwUpdate, private swPush: SwPush) { }
 
   ngOnInit() {
+    
   }
   toggleMainMenu() {
     this.isMainCollapsed = !this.isMainCollapsed;
@@ -25,6 +29,14 @@ export class NavHeaderComponent implements OnInit {
 
   toggleMenu() {
     this.toggleMenuBar = this.toggleMenuBar === 'colapseMenuBar' ? 'expandMenuBar' : 'colapseMenuBar';
+  }
+
+  subscribe() {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => this.navHeaderService.addPushSubscriber(sub).subscribe())
+      .catch(err => console.error('Could not subscribe to notifications', err));
   }
 
   hideMenu() { this.menuVisible = false; }
--- src/app/shared/nav-header/nav-header.service.ts
+++ src/app/shared/nav-header/nav-header.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { AppSetting } from '../../config/appSetting';
 
 @Injectable()
 export class NavHeaderService {
     menuTransparent: string;
+    serviceUrl: string = AppSetting.serviceUrl;
+    constructor(private http: HttpClient) {
+
+    }
     makeMenuTransparent() {
         this.menuTransparent = 'menuTransparent';
     }
 
     hideMenuTransparent() {
         this.menuTransparent = '';
+    }
+
+    addPushSubscriber(sub: any) {
+        const notificationUrl = 'notification';
+        const url: string = this.serviceUrl + notificationUrl;
+        return this.http.post(url, sub);
     }
 }
