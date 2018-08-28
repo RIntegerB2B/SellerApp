85f626aab18f37435bce2641148c50ff5c804d91
Chandran <Preveen_Chandran@Dell.com>
2018-08-28 16:12:24 +0530

    ENV Changes

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,4 +1,4 @@
-import { environment } from "../../environments/environment";
+import { environment } from '../../environments/environment';
 export const AppSetting: AppSettingType = {
   awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/',
   local3010BuyerServiceUrl: 'http://localhost:3001/',
